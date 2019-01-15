6a786b708887cdbc5cfd830751abe0db5e996c30
Feng Lee <feng@emqx.io>
2019-01-15 22:07:21 +0800

    Start emqx cluster without epmd
    - See http://erlang.org/pipermail/erlang-questions/2015-December/087013.html
    - Upgrade 'bin/nodetool' to compile 'nodetool_epmd' module at runtime

--- bin/emqx_ctl.cmd
+++ bin/emqx_ctl.cmd
@@ -43,7 +43,7 @@
   copy "%rel_dir%\%rel_name%.boot" "%rel_dir%\start.boot" >nul
 )
 
-@%escript% %nodetool% %node_type% "%node_name%" -setcookie "%node_cookie%" rpc emqx_ctl run_command %args%
+@%escript% %nodetool% %node_type% "%node_name%" -setcookie "%node_cookie%" -start_epmd true rpc emqx_ctl run_command %args%
 
 :: Find the ERTS dir
 :find_erts_dir
